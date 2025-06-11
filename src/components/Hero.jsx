b2f0c4eefefe912e12cf59d9b7c6681ca0e57eff
Hazel Baldava <hazelbaldava016@gmail.com>
2025-06-12 03:58:29 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
         {/* Profile Picture */}
         <div className="flex justify-center mb-8">
           <img
-            src="https://scontent-mnl1-1.xx.fbcdn.net/v/t39.30808-6/465745115_8977811638942854_5499499755299702046_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeEEoAzyAYM2biKYyd_2_8WgYxTIzlP-Qv5jFMjOU_5C_hpUlnCKNOslbJHAfREXtYStoNTgXSkdJNP3MgMZknwF&_nc_ohc=PA9i3LR2lkcQ7kNvwHIkRUA&_nc_oc=AdkOWJo1ga7vnQdjtKg3QjcgLN3OHDr_7BRhmfrhxEgEFv10EZdD01pkorKoXQ4VR7A&_nc_zt=23&_nc_ht=scontent-mnl1-1.xx&_nc_gid=k-TPIx3vieXeaowYtptTjA&oh=00_AfM4B-EJF5HZXaZOAFWCr-bLVkd9srOSJWbGY2mQ2KZ0Fg&oe=6852E4A3"
+            src="https://scontent-mnl3-2.xx.fbcdn.net/v/t39.30808-6/457458544_8478062415584448_5766051916160929740_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHA5DyvbTwmR8XW1gvX4I7P-Gr2FwLSP7j4avYXAtI_uLNViw8MJTS3Y9cbFr9NUB2gS_5TtswkrucjmR9UhBGT&_nc_ohc=LeSN-4Xf7AsQ7kNvwEm1mzQ&_nc_oc=AdlNdPelmSYHX8U-3CUKemnFnalwYsCOemb-45aGDGb_UeKjiyY9AIwHKheE5iEdpkw&_nc_zt=23&_nc_ht=scontent-mnl3-2.xx&_nc_gid=5NRdCVKeJlYw079Pi68rHg&oh=00_AfMDILM1jlZniJhcDeiZb1EKDxXbcc0J6eOim8YZBBoMMA&oe=6852E71C"
             alt="Hazel Baldava"
             className="w-40 h-40 rounded-full object-cover border-4 border-white shadow-lg"
           />
